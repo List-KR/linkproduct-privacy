462719495d46896508ae71b809a01661d2bb0486
PiQuark6046 <piquark6046@proton.me>
2023-07-27 05:26:01 +0000

    feat: add automatic URL parser to clean metadata

--- linkproduct-privacy.user.js
+++ linkproduct-privacy.user.js
@@ -8,7 +8,7 @@
 // @downloadURL  https://cdn.jsdelivr.net/gh/List-KR/linkproduct-privacy@main/linkproduct-privacy.user.js
 // @license      MIT
 //
-// @version      1.0.1
+// @version      1.1.0
 // @author       PiQuark6046 and contributors
 //
 // @match        *://*/*
@@ -17,14 +17,90 @@
 // @description:ko     linkproduct-privacy는 인플루언서 마케팅 URL에서 원본 URL을 얻을 수 있습니다.
 //
 // @grant        unsafeWindow
-// @grant        GM.xmlhttpRequest
+// @grant        GM_xmlhttpRequest
 // @run-at       document-end
 // ==/UserScript==
 (function () {
     const GMXmlhttpRequest = typeof GM.xmlhttpRequest !== 'undefined' ? GM.xmlhttpRequest : GM_xmlhttpRequest;
     const LinkProductURLs = [
         '//app.ac/',
-        '//link.coupang.com/a/'
+        '//link.coupang.com/a/',
+        '//link.coupang.com/re/',
+        '//qoo.tn/'
+    ];
+    const LinkResultURLs = [
+        {
+            URLPattern: /^https:\/\/[a-z]+\.gmarket\.co\.kr\/linkprice\/lpfront\.asp/,
+            ModificationFunction: function (ResultURL) {
+                let SearchParamsURL = new URL(ResultURL).searchParams.get('url');
+                return /^https:\/\/m?item\.gmarket\.co\.kr\/(detailview\/)?Item(\.asp)?\?goodscode=[0-9]+/.exec(SearchParamsURL)[0];
+            }
+        },
+        {
+            URLPattern: /^https:\/\/[a-z]+\.wemakeprice\.com\/product\/[0-9]+/,
+            ModificationFunction: function (ResultURL) {
+                let Oringin = new URL(ResultURL).origin;
+                let Pathname = new URL(ResultURL).pathname;
+                return Oringin + Pathname;
+            }
+        },
+        {
+            URLPattern: /^https:\/\/[a-z]+\.coupang\.com\/vp\/products\/[0-9]+/,
+            ModificationFunction: function (ResultURL) {
+                let Oringin = new URL(ResultURL).origin;
+                let Pathname = new URL(ResultURL).pathname;
+                return Oringin + Pathname;
+            }
+        },
+        {
+            URLPattern: /^https?:\/\/m?item(page[0-9]+)?\.auction\.co\.kr\/[A-z]+(\.[a-z]+)?\?item(N|n)o=[A-z0-9]+/,
+            ModificationFunction: function (ResultURL) {
+                return /^https?:\/\/m?item(page[0-9]+)?\.auction\.co\.kr\/[A-z]+(\.[a-z]+)?\?item(N|n)o=[A-z0-9]+/.exec(ResultURL)[0];
+            }
+        },
+        {
+            URLPattern: /^https:\/\/(www|m)\.qoo10\.com\/g\/[0-9]+/,
+            ModificationFunction: function (ResultURL) {
+                return /^https:\/\/(www|m)\.qoo10\.com\/g\/[0-9]+/.exec(ResultURL)[0];
+            }
+        },
+        {
+            URLPattern: /^https:\/\/(www|m)\.e-himart\.co\.kr\/common\/include\/ipfront.jsp\?lpinfo=/,
+            ModificationFunction: function (ResultURL) {
+                return new URL(ResultURL).searchParams.get('url');
+            }
+        },
+        {
+            URLPattern: /^https?:\/\/(www|m)\.11st\.co\.kr\/connect\/Gateway\.tmall\?apInfo=.+&lpUrl=/,
+            ModificationFunction: function (ResultURL) {
+                let LpURL = new URL(ResultURL).searchParams.get('lpUrl');
+                return new URL(LpURL).search === '' ? LpURL : new URL(LpURL).origin + new URL(LpURL).pathname;
+            }
+        },
+        {
+            URLPattern: /^https:\/\/[a-z]+\.coupang\.com\/np\/campaigns\/[0-9]+/,
+            ModificationFunction: function (ResultURL) {
+                let Oringin = new URL(ResultURL).origin;
+                let Pathname = new URL(ResultURL).pathname;
+                return Oringin + Pathname;
+            }
+        },
+        {
+            URLPattern: /^https:\/\/pages\.coupang\.com\/p\/[A-z0-9]+/,
+            ModificationFunction: function (ResultURL) {
+                let Oringin = new URL(ResultURL).origin;
+                let Pathname = new URL(ResultURL).pathname;
+                return Oringin + Pathname;
+            }
+        },
+        {
+            URLPattern: /^https:\/\/[a-z]+\.lotteon\.com\/m\/product\/[A-z0-9]+/,
+            ModificationFunction: function (ResultURL) {
+                let Oringin = new URL(ResultURL).origin;
+                let Pathname = new URL(ResultURL).pathname;
+                return Oringin + Pathname;
+            }
+        }
     ];
     let LinkElements = document.querySelectorAll(LinkProductURLs.map(function (Value) { return `a[href*="${Value}"]`; }).join(', '));
     console.debug('linkproduct-privacy: LinkElements: ', LinkElements);
@@ -35,9 +111,25 @@
             continue;
         }
         GMXmlhttpRequest({ url: URLAddress, method: 'GET', responseType: 'document', anonymous: true, onload: function (ResponseObject) {
-                LinkElement.setAttribute('href', ResponseObject.finalUrl);
-                LinkElement.innerText = ResponseObject.finalUrl;
-                console.debug('linkproduct-privacy: Completed for:', { 'Element': LinkElement, 'Affiliate marketing URL': URLAddress });
+                let UpdateURL;
+                for (let i = 0; i < LinkResultURLs.length; i++) {
+                    if (LinkResultURLs[i].URLPattern.test(ResponseObject.finalUrl)) {
+                        UpdateURL = LinkResultURLs[i].ModificationFunction(ResponseObject.finalUrl);
+                        console.debug('linkproduct-privacy: The reponse URL matches a predefined URL:', {
+                            'Element': LinkElement, 'Affiliate marketing URL': URLAddress, 'Response URL': ResponseObject.finalUrl, 'Processed URL': UpdateURL
+                        });
+                        break;
+                    }
+                    if (LinkResultURLs.length - 1 === i) {
+                        UpdateURL = ResponseObject.finalUrl;
+                        console.warn('linkproduct-privacy: The reponse URL does NOT match any predefined URL:', {
+                            'Element': LinkElement, 'Affiliate marketing URL': URLAddress, 'Response URL': ResponseObject.finalUrl
+                        });
+                    }
+                }
+                LinkElement.setAttribute('href', UpdateURL);
+                if (/^(http(s|):)?\/\/.+\..+\/.+/.test(LinkElement.innerText))
+                    LinkElement.innerText = UpdateURL;
             } });
     }
 })();
--- linkproduct-privacy.user.ts
+++ linkproduct-privacy.user.ts
@@ -8,7 +8,7 @@
 // @downloadURL  https://cdn.jsdelivr.net/gh/List-KR/linkproduct-privacy@main/linkproduct-privacy.user.js
 // @license      MIT
 //
-// @version      1.0.2
+// @version      1.1.0
 // @author       PiQuark6046 and contributors
 //
 // @match        *://*/*
@@ -71,11 +71,92 @@ declare const GM: GM
 type GM_xmlhttpRequest = typeof GM.xmlhttpRequest
 declare const GM_xmlhttpRequest: GM_xmlhttpRequest
 
+interface LinkResultURL {
+  URLPattern: RegExp,
+  ModificationFunction: (ResultURL: string) => string
+}
+
 (function () {
   const GMXmlhttpRequest = typeof GM.xmlhttpRequest !== 'undefined' ? GM.xmlhttpRequest : GM_xmlhttpRequest
   const LinkProductURLs = [
     '//app.ac/',
-    '//link.coupang.com/a/'
+    '//link.coupang.com/a/',
+    '//link.coupang.com/re/',
+    '//qoo.tn/'
+  ]
+  const LinkResultURLs: Array<LinkResultURL> = [
+    {
+      URLPattern: /^https:\/\/[a-z]+\.gmarket\.co\.kr\/linkprice\/lpfront\.asp/,
+      ModificationFunction: function (ResultURL: string) {
+        let SearchParamsURL = new URL(ResultURL).searchParams.get('url')
+        return /^https:\/\/m?item\.gmarket\.co\.kr\/(detailview\/)?Item(\.asp)?\?goodscode=[0-9]+/.exec(SearchParamsURL)[0]
+      }
+    },
+    {
+      URLPattern: /^https:\/\/[a-z]+\.wemakeprice\.com\/product\/[0-9]+/,
+      ModificationFunction: function (ResultURL: string) {
+        let Oringin = new URL(ResultURL).origin
+        let Pathname = new URL(ResultURL).pathname
+        return Oringin + Pathname
+      }
+    },
+    {
+      URLPattern: /^https:\/\/[a-z]+\.coupang\.com\/vp\/products\/[0-9]+/,
+      ModificationFunction: function (ResultURL: string) {
+        let Oringin = new URL(ResultURL).origin
+        let Pathname = new URL(ResultURL).pathname
+        return Oringin + Pathname
+      }
+    },
+    {
+      URLPattern: /^https?:\/\/m?item(page[0-9]+)?\.auction\.co\.kr\/[A-z]+(\.[a-z]+)?\?item(N|n)o=[A-z0-9]+/,
+      ModificationFunction: function (ResultURL: string) {
+        return /^https?:\/\/m?item(page[0-9]+)?\.auction\.co\.kr\/[A-z]+(\.[a-z]+)?\?item(N|n)o=[A-z0-9]+/.exec(ResultURL)[0]
+      }
+    },
+    {
+      URLPattern: /^https:\/\/(www|m)\.qoo10\.com\/g\/[0-9]+/,
+      ModificationFunction: function (ResultURL: string) {
+        return /^https:\/\/(www|m)\.qoo10\.com\/g\/[0-9]+/.exec(ResultURL)[0]
+      }
+    },
+    {
+      URLPattern: /^https:\/\/(www|m)\.e-himart\.co\.kr\/common\/include\/ipfront.jsp\?lpinfo=/,
+      ModificationFunction: function (ResultURL: string) {
+        return new URL(ResultURL).searchParams.get('url')
+      }
+    },
+    {
+      URLPattern: /^https?:\/\/(www|m)\.11st\.co\.kr\/connect\/Gateway\.tmall\?apInfo=.+&lpUrl=/,
+      ModificationFunction: function (ResultURL: string) {
+        let LpURL = new URL(ResultURL).searchParams.get('lpUrl')
+        return new URL(LpURL).search === '' ? LpURL : new URL(LpURL).origin + new URL(LpURL).pathname
+      }
+    },
+    {
+      URLPattern: /^https:\/\/[a-z]+\.coupang\.com\/np\/campaigns\/[0-9]+/,
+      ModificationFunction: function (ResultURL: string) {
+        let Oringin = new URL(ResultURL).origin
+        let Pathname = new URL(ResultURL).pathname
+        return Oringin + Pathname
+      }
+    },
+    {
+      URLPattern: /^https:\/\/pages\.coupang\.com\/p\/[A-z0-9]+/,
+      ModificationFunction: function (ResultURL: string) {
+        let Oringin = new URL(ResultURL).origin
+        let Pathname = new URL(ResultURL).pathname
+        return Oringin + Pathname
+      }
+    },
+    {
+      URLPattern: /^https:\/\/[a-z]+\.lotteon\.com\/m\/product\/[A-z0-9]+/,
+      ModificationFunction: function (ResultURL: string) {
+        let Oringin = new URL(ResultURL).origin
+        let Pathname = new URL(ResultURL).pathname
+        return Oringin + Pathname
+      }
+    }
   ]
   let LinkElements = document.querySelectorAll(LinkProductURLs.map(function(Value) { return `a[href*="${Value}"]` }).join(', ')) as NodeListOf<HTMLAnchorElement>
   console.debug('linkproduct-privacy: LinkElements: ', LinkElements)
@@ -86,9 +167,24 @@ declare const GM_xmlhttpRequest: GM_xmlhttpRequest
       continue
     }
     GMXmlhttpRequest({url: URLAddress, method: 'GET', responseType: 'document', anonymous: true, onload: function (ResponseObject) {
-      LinkElement.setAttribute('href', ResponseObject.finalUrl)
-      LinkElement.innerText = ResponseObject.finalUrl
-      console.debug('linkproduct-privacy: Completed for:', { 'Element': LinkElement, 'Affiliate marketing URL': URLAddress })
+      let UpdateURL: string
+      for (let i = 0; i < LinkResultURLs.length; i++) {
+        if (LinkResultURLs[i].URLPattern.test(ResponseObject.finalUrl)) {
+          UpdateURL = LinkResultURLs[i].ModificationFunction(ResponseObject.finalUrl)
+          console.debug('linkproduct-privacy: The reponse URL matches a predefined URL:', {
+            'Element': LinkElement, 'Affiliate marketing URL': URLAddress, 'Response URL': ResponseObject.finalUrl, 'Processed URL': UpdateURL
+          })
+          break
+        }
+        if (LinkResultURLs.length - 1 === i) {
+          UpdateURL = ResponseObject.finalUrl
+          console.warn('linkproduct-privacy: The reponse URL does NOT match any predefined URL:', {
+            'Element': LinkElement, 'Affiliate marketing URL': URLAddress, 'Response URL': ResponseObject.finalUrl
+          })
+        }
+      }
+      LinkElement.setAttribute('href', UpdateURL)
+      if (/^(http(s|):)?\/\/.+\..+\/.+/.test(LinkElement.innerText)) LinkElement.innerText = UpdateURL
     }})
   }
 })()
