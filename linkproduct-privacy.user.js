d103bc2a8f05c5904f987f65317c449af08167eb
fifoqueue <me@hsr.vg>
2026-08-31 00:06:09 +0900

    chore: version bump

--- linkproduct-privacy.user.js
+++ linkproduct-privacy.user.js
@@ -8,7 +8,7 @@
 // @downloadURL  https://cdn.jsdelivr.net/gh/List-KR/linkproduct-privacy@main/linkproduct-privacy.user.js
 // @license      MIT
 //
-// @version      2.0.3
+// @version      2.0.4
 // @author       List-KR
 //
 // @match        *://*/*
